0aa5476780d7d1b39d3e761d46a44d2b7228a759
Kaljarys <kbrantone@gmail.com>
2019-12-03 18:38:36 +0100

    interface is bascially done, working on the script

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function add(a, b){
+    let result = a + b; 
+    return result;
+  }
+  function divise(a, b){
+    let result = a / b; 
+    return result; 
+  }
+  function substract(a, b){
+    let result = a - b; 
+    return result; 
+  }
+  function multiplicate(a, b){
+    let result = a * b; 
+    return result; 
+  }
+  var input1 = ""; 
+  var input2 = "";
+  
